f66e8721238c98a146ce94d6f6fe1919b2cb9dc5
joe0the0slug <116825220+joe0the0slug@users.noreply.github.com>
2023-12-12 11:12:26 +0000

    Added function to initialse computer grid

--- BattleBoats/Program.cs
+++ BattleBoats/Program.cs
@@ -12,6 +12,7 @@ namespace BattleBoats
 
             char[,] ownGrid = new char[8, 8];
             char[,] hitsAndMissesGrid = new char[8, 8];
+            char[,] computerGrid = new char[8, 8];
             string mode = "";
 
 
@@ -19,7 +20,7 @@ namespace BattleBoats
             {
                 mode =DisplayMenu();
             }
-            ownGrid =IntialiseGrid(ownGrid);
+            ownGrid =IntialisePlayerGrid(ownGrid);
 
             int[] arrayCoodinateas = {-1,-1};
             while (arrayCoodinateas[0] == -1) 
@@ -29,8 +30,9 @@ namespace BattleBoats
                 arrayCoodinateas = GetCoodrinates(coordinate);
                 foreach (int i in arrayCoodinateas) { Console.WriteLine(i); }
             }
+            computerGrid = IntialiseComputerGrid(computerGrid);
 
-
+            
 
 
             //Console.WriteLine(ownGrid[arrayCoodinateas[0],arrayCoodinateas[1]]);
@@ -140,39 +142,80 @@ namespace BattleBoats
             }
         }
 
-        static char[,] IntialiseGrid(char[,] grid)
-        //playerGrid?
+        static char[,] IntialisePlayerGrid(char[,] playerGrid)
+        //where do I want to  initialise the computer grid?
         {
             // if I add different types of boats I am going to have to significantly change this
             int numberOfBoats = 5;
 
-            for(int i = 0;i < grid.GetLength(0); i++)
+            for(int i = 0;i < playerGrid.GetLength(0); i++)
             {
-                for(int y = 0 ; y < grid.GetLength(1); y++)
+                for(int y = 0 ; y < playerGrid.GetLength(1); y++)
                 {
-                    grid[i, y] = '*';
+                    playerGrid[i, y] = '*';
                 }
             }
 
-            DisplayGrid(grid);
+            DisplayGrid(playerGrid);
 
-            for(int i = 1; i < numberOfBoats; i++)
+            for(int i = 1; i <= numberOfBoats; i++)
             {
                 int[] boatCoordinates = new int[2];
                 int[] inputCoordinates = { -1, -1 };
-                while (inputCoordinates[0] == -1 || grid[inputCoordinates[1],inputCoordinates[0]] == 'B') 
+                while (inputCoordinates[0] == -1 || playerGrid[inputCoordinates[1],inputCoordinates[0]] == 'B') 
                 // -1 if invalid input coordinates and if they attempt to place one on top of a boat
                 {
                     Console.WriteLine($"enter your coordinates in the form: NumberLetter for the placment of boat number {i} (only place boats in empty spaces):");
                     inputCoordinates = GetCoodrinates(Console.ReadLine());
                 }
-                grid[inputCoordinates[1],inputCoordinates[0]] = 'B';
+                playerGrid[inputCoordinates[1],inputCoordinates[0]] = 'B';
                 Console.WriteLine("Boat placed!");
-                DisplayGrid(grid);
+                DisplayGrid(playerGrid);
             }
             
 
-            return grid;
+            return playerGrid;
+        }
+
+
+        static char[,] IntialiseComputerGrid(char[,] computerGrid)
+        //where do I want to  initialise the computer grid?
+        {
+            // if I add different types of boats I am going to have to significantly change this
+            int numberOfBoats = 5;
+
+            for (int i = 0; i < computerGrid.GetLength(0); i++)
+            {
+                for (int y = 0; y < computerGrid.GetLength(1); y++)
+                {
+                    computerGrid[i, y] = '*';
+                }
+            }
+
+
+
+            Random rand = new Random();
+
+            for (int i = 1; i <= numberOfBoats; i++)
+            {
+                int[] boatCoordinates = new int[2];
+                int[] inputCoordinates = new int[2];
+                do 
+                // -1 if invalid input coordinates and if they attempt to place one on top of a boat
+                {
+                    inputCoordinates[1] = rand.Next(computerGrid.GetLength(0));
+                    inputCoordinates[0] = rand.Next(computerGrid.GetLength(1));
+                }
+                while (computerGrid[inputCoordinates[1], inputCoordinates[0]] == 'B') ;
+
+
+                computerGrid[inputCoordinates[1], inputCoordinates[0]] = 'B';
+                Console.WriteLine("Boat placed! on computer");
+                DisplayGrid(computerGrid);
+            }
+
+  
+            return computerGrid;
         }
 
 
